7e5ec6462575ffcc44a600061874a33e6a961ba9
MoriartyCoder <DanTronnier@hotmail.de>
2023-02-03 19:15:31 +0100

    Client kann jetzt die Eingaben an den Server schicken

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ PORT = 5000  # socket server port number
 
 #Erste Aufgabe variablen
 minimum = 1
-message1= ""
+data_string= ""
 #---------------------------------------------------------------------------
 
 print("Start Client")
@@ -16,26 +16,26 @@ client_socket = socket.socket()  # instantiate
 client_socket.connect((HOST, PORT))  # connect to the server
 
 message = input(" -> ")  # take input
-if message == "number":
-
-    maximum = int(input("Maximum -> "))
-    minimum = int(input("Minimum -> "))
-    message1 = " "
-
-    message1 += "Max:" + str(maximum) + ";"
-    message1 += "Min:" + str(minimum) + ";"
-
-    print(message1)
-
-
 
 while message.lower().strip() != 'bye':
-    client_socket.send(message.encode())  # send message
-    client_socket.send(message1.encode())  # send message
 
-    data = client_socket.recv(1024).decode()  # receive response
+    if message == "number":
+        maximum = int(input("Maximum -> "))
+        minimum = int(input("Minimum -> "))
+        data_string = ""
+
+        data_string += "Max:" + str(maximum) + ";"
+        data_string += "Min:" + str(minimum) + ";"
+
+        print(data_string)
+
+        client_socket.send(data_string.encode())  # send message
+
+    else:
+        client_socket.send(message.encode())  # send message
+
     num = client_socket.recv(1024).decode()  # receive response
-    print('Received from server: ' + data)  # show in terminal
+    print('Received number from server: ' + num)  # show in terminal
 
     message = input(" -> ")  # again take input
 
