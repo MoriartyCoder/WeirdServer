33c2bafa18acbfc0a4a671f80e1d7d6eb29cd4d3
Maggitum <zsofi.balo8@gmail.com>
2023-02-03 21:44:32 +0100

    Server kann zufallszahl zurückschicken.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import random
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -19,20 +20,35 @@ conn, address = server_socket.accept()  # accept new connection
 print("Connection from: " + str(address))
 while True:
     # receive data stream. it won't accept data packet greater than 1024 bytes
-    data = conn.recv(1024).decode()
+    data = conn.recv(1024).decode().lower()
     if not data:
         # if data is not received break
         break
     print("from connected user: " + str(data))
     print()
-    data = input(' -> ')
+    y = data.split(";")
+    split_string = []
+    for i in y:
+        split_string += i.split(":")
+    print(split_string)
+    print(y)
+    i = 0
+    max=None
+    min=1
+    while i < len(split_string):
+        print(split_string[i])
+        if split_string[i] == "max":
+            max = int(split_string[i+1])
+        if split_string[i] == "min":
+            min = int(split_string[i+1])
+        i += 2
+
+    print(max)
+    print(min)
+
+    data = str(random.randrange(min, max))
+    print(data)
+
     conn.send(data.encode())  # send data to the client
 
-    num = conn.recv(1024).decode()
-    if not num:
-        break
-    print("From user: " + str(num))
-    print('Received from server: ' + num)  # show in terminal
-    print(num)
-    conn.send(num.encode())  # send data to the client
 conn.close()  # close the connection
